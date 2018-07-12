06fa030c2fd75f58def1526ea32132c3a372a489
MAE <MAE@776569.com>
2018-07-12 16:48:06 +0200

    Add custom password help text

--- src/authentication/forms.py
+++ src/authentication/forms.py
@@ -2,22 +2,38 @@ from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import get_user_model, password_validation
 from django.core.validators import validate_email
+from django.utils.html import format_html
 
 class LoginForm(AuthenticationForm):
-    email = forms.EmailField(required=True, label="Email",
+    username = forms.EmailField(required=True, label="Email",
                 validators=[validate_email])
+
+    password = forms.CharField(
+        label="Password",
+        strip=True,
+        widget=forms.PasswordInput,
+    )
 
     FIELD_NAME_MAPPING = {"username": "email"}
     def add_prefix(self, field_name):
         field_name = self.FIELD_NAME_MAPPING.get(field_name, field_name)
         return super().add_prefix(field_name)
 
+def get_password_help_text():
+    help_texts = password_validation.password_validators_help_texts()
+    help_texts.append("""Password must contain at least one uppercase letter
+        one lowercase letter, digit and special character.""")
+    help_texts.append("The spaces at the start and the end will be trimmed.")
+    help_items = [format_html('<li>{}</li>', help_text) for help_text in help_texts]
+    return '<ul class="help-text-list">%s</ul>' % ''.join(help_items) if help_items else ''
+
+
 class RegisterationForm(UserCreationForm):
     password1 = forms.CharField(
         label="Password",
         strip=True,
         widget=forms.PasswordInput,
-        help_text=password_validation.password_validators_help_text_html(),
+        help_text=get_password_help_text(),
     )
     password2 = forms.CharField(
         label="Password confirmation",
