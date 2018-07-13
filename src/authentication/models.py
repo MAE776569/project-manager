9dc62d7a507a280542833c4da4df4866c6fb1ed2
MAE <MAE@776569.com>
2018-07-13 14:44:38 +0200

    Modified account verification to be robust

--- src/authentication/models.py
+++ src/authentication/models.py
@@ -1,39 +1,46 @@
 from django.db import models
 from os import urandom
-from time import time
 import hmac
 from hashlib import sha256
+from uuid import uuid4
 
 def get_salt():
     return urandom(16).hex()
 
-def get_timestamp():
-    return str(time())
+HASH_SEPARATOR = b'#'
 
 class AccountVerification(models.Model):
     name = models.CharField(max_length=100, blank=False)
     email = models.EmailField(unique=True, blank=False)
-    verification_code = models.CharField(max_length=100, blank=True)
+    verification_code = models.CharField(max_length=64, blank=True)
     salt = models.CharField(max_length=32, blank=True, default=get_salt)
-    time_stamp = models.CharField(max_length=32, blank=True, default=get_timestamp)
+    uuid = models.UUIDField(unique=True, blank=True, default=uuid4)
     verified = models.BooleanField(blank=True, default=False)
 
     def get_verification_code(self):
         if not self.verification_code:
-            verification_code = urandom(17).hex()
-            hashed_code = hmac.new(
-                str(self.salt).encode() + str(self.time_stamp).encode(),
-                msg=str(verification_code).encode(),
+            verification_code = urandom(32).hex()
+            user_msg = self.salt.encode() + HASH_SEPARATOR + self.verification_code.encode()
+            hashed_user_code = hmac.new(
+                str(self.uuid).encode(),
+                msg=user_msg,
                 digestmod=sha256)
-            self.verification_code = hashed_code.hexdigest()
-            return verification_code
+            hashed_user_code = hashed_user_code.hexdigest()
+            msg = self.salt.encode() + HASH_SEPARATOR + hashed_user_code.encode()
+            saved_code = hmac.new(
+                str(self.uuid).encode(),
+                msg=msg,
+                digestmod=sha256)
+            self.verification_code = saved_code.hexdigest()
+            return hashed_user_code
         else:
             raise ValueError("account already have a verification code")
 
     def verify_code(self, user_code):
+        user_msg = self.salt.encode() + HASH_SEPARATOR + user_code.encode()
         hashed_code = hmac.new(
-                str(self.salt).encode() + str(self.time_stamp).encode(),
-                msg=str(user_code).encode(),
+                str(self.uuid).encode(),
+                msg=user_msg,
                 digestmod=sha256)
         return hmac.compare_digest(self.verification_code, hashed_code.hexdigest())
 
