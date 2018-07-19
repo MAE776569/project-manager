e57fe96b37fcd1f55a0f12086b7aca2b51142d89
MAE <MAE@776569.com>
2018-07-19 14:55:09 +0200

    Add admin app

--- src/project-manager/settings/base.py
+++ src/project-manager/settings/base.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'authentication'
+    'authentication',
+    'administrator'
 ]
 
 MIDDLEWARE = [
--- src/project-manager/urls.py
+++ src/project-manager/urls.py
@@ -21,11 +21,13 @@ from accounts import urls as accounts_urls
 from authentication.views import IndexView
 from django.conf import settings
 from django.conf.urls.static import static
+from administrator import urls as admin_urls
 
 urlpatterns = [
     re_path(r"^$", IndexView.as_view(), name="index"),
     re_path(r"^auth/", include((auth_urls, 'authentication'), namespace="auth")),
     re_path(r"^accounts/", include((accounts_urls, 'accounts'), namespace="accounts")),
+    re_path(r"^admin/", include((admin_urls, 'admin'), namespace="admin"))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
