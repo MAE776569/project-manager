86f6342ffb060dffcaa25ff7a28240b478ff0a29
MAE <MAE@776569.com>
2018-07-30 18:40:42 +0200

    Create resources models

--- src/resources/models.py
+++ src/resources/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Link(models.Model):
+    title = models.CharField(blank=False, max_length=100)
+    subtitle = models.CharField(blank=False, max_length=250)
+    url = models.URLField(blank=False)
+
+    class Meta:
+        db_table = 'link'
+        verbose_name = 'link'
+        verbose_name_plural = 'links'
+
+class Document(models.Model):
+    title = models.CharField(blank=False, max_length=100)
+    subtitle = models.CharField(blank=False, max_length=250)
+    document = models.FileField(blank=False, upload_to='documents')
+
+    class Meta:
+        db_table = 'document'
+        verbose_name = 'document'
+        verbose_name_plural = 'documents'
